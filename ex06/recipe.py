c52441b29c54e5ca3b95abca24e8e665b9ecd2ed
Trojanforyou <81333426+Trojanforyou@users.noreply.github.com>
2025-08-03 22:53:32 +0200

    update

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -1,27 +1,62 @@
 #! /usr/bin/env python3
 
-cookbook = {
-    'ingredients': [],
-    'meal': "",
-    'prep_time': 0,
-    'name': "",
-
-}
+cookbook = {}
 
 def add_recep():
     ingredients = []
     tmp = ""
-    cookbook['name'] = input("Enter a name:\n")
+    name = input("Enter a name:\n")
     print("Enter ingredience")
     while (True):
         tmp = input()
         if (tmp == ""):
             break
         ingredients.append(tmp)
-    cookbook["ingredients"] = ingredients
-    cookbook['meal'] = input("Enter a meal type:\n")
-    cookbook['prep_time'] = input("Enter a preparation time:\n")
-add_recep()
+    meal = input("Enter a meal type:\n")
+    prep_time= input("Enter a preparation time:\n")
+
+    cookbook[name] = {
+    'ingredients': ingredients,
+    'meal': meal,
+    'prep_time': prep_time,
+}
+
+def print_res():
+    user = input("Please Enter a name: ")
+    if (user in cookbook):
+        recipe = cookbook[user]
+        print(f"Recipie for {user}")
+        print(f"Ingredients list: {recipe['ingredients']}")
+        print(recipe["meal"])
+        print(recipe["prep_time"])
+    else:
+        print("Sorry, but there's no such a recipe")
+        return
+def print_book():
+    if (not cookbook):
+        print("Cookbook is empty")
+    else:
+        for name, recipe in cookbook.items():
+            print(f"\nRecipe: {name}")
+            print(f"   Ingredients: {recipe['ingredients']}")
+            print(f"   Meal: {recipe['meal']}")
+            print(f"    Prep time: {recipe['prep_time']}")
+def main():
+    print("List of available options: ")
+    print("1: Add a recipe\n2: Delete a recipe\n3: Print recipe\n4: Print the cookbook\n5: Quit\n\n")
+    while (True):
+        user = ""
+        user = input("Please select the option: ")
+        if (user == "1"):
+            add_recep()
+        elif (user == "3"):
+            print_res()
+        elif (user == "4"):
+           print_book()
+        elif (user == "5"):
+            print("\nCookbook closed. Goodbye !")
+            break
+main()
     
 
 
